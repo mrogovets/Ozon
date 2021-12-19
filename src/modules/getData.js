ef94685e472b9d3aee2b337932e0f82bce6a3b1f
mrogovets <mrogovets@ukr.net>
2021-12-19 22:40:08 +0200

    after 3-d day

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,6 +1,8 @@
-const getData = () => {
+const getData = (str) => {
   return fetch(
-    "https://o-zone-558ee-default-rtdb.firebaseio.com/goods.json"
+    `https://o-zone-558ee-default-rtdb.firebaseio.com/goods.json?${
+      str ? `search=${str}` : ""
+    }`
   ).then((response) => {
     return response.json();
   });
