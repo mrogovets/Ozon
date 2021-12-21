c7f610ee0cdb5b91cc3d07cba02f6ca3abc98686
mrogovets <mrogovets@ukr.net>
2021-12-21 14:36:19 +0200

    after 4-th day

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,10 +1,24 @@
+import renderCart from "./renderCart";
+import postData from "./postData";
+
 const cart = () => {
   const cartBtn = document.getElementById("cart");
   const cartModal = document.querySelector(".cart");
-  const cartCloseBtn = document.querySelector(".cart-close");
+  const cartCloseBtn = cartModal.querySelector(".cart-close");
+  const cartTotal = cartModal.querySelector(".cart-total > span");
+  const cartSendBtn = cartModal.querySelector(".cart-confirm");
+  const goodsWrapper = document.querySelector(".goods");
+  const cartWrapper = document.querySelector(".cart-wrapper");
 
   const openCart = () => {
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
     cartModal.style.display = "flex";
+    renderCart(cart);
+    cartTotal.textContent = cart.reduce((sum, goodsItem) => {
+      return sum + goodsItem.price;
+    }, 0);
   };
 
   const closeCart = () => {
@@ -13,6 +27,55 @@ const cart = () => {
 
   cartBtn.addEventListener("click", openCart);
   cartCloseBtn.addEventListener("click", closeCart);
+
+  goodsWrapper.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-primary")) {
+      const card = event.target.closest(".card");
+      const key = card.dataset.key;
+      const goods = JSON.parse(localStorage.getItem("goods"));
+      const cart = localStorage.getItem("cart")
+        ? JSON.parse(localStorage.getItem("cart"))
+        : [];
+      const goodsItem = goods.find((item) => {
+        return item.id === +key;
+      });
+
+      cart.push(goodsItem);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+  cartWrapper.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-primary")) {
+      const cart = localStorage.getItem("cart")
+        ? JSON.parse(localStorage.getItem("cart"))
+        : [];
+      const card = event.target.closest(".card");
+      const key = card.dataset.key;
+      const index = cart.findIndex((item) => {
+        return item.id === +key;
+      });
+
+      cart.splice(index, 1);
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      renderCart(cart);
+      cartTotal.textContent = cart.reduce((sum, goodsItem) => {
+        return sum + goodsItem.price;
+      }, 0);
+    }
+  });
+  cartSendBtn.addEventListener("click", () => {
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+
+    postData(cart).then(() => {
+      localStorage.removeItem("cart");
+      renderCart([]);
+      cartTotal.textContent = 0;
+    });
+  });
 };
 
 export default cart;
--- src/modules/renderCart.js
+++ src/modules/renderCart.js
@@ -1,15 +1,18 @@
-const renderGoods = (goods) => {
-  const goodsWrapper = document.querySelector(".goods");
+const renderCart = (goods) => {
+  const cartWrapper = document.querySelector(".cart-wrapper");
 
-  localStorage.setItem("goods", JSON.stringify(goods));
+  cartWrapper.innerHTML = "";
 
-  goodsWrapper.innerHTML = "";
-
-  goods.forEach((goodsItem) => {
-    goodsWrapper.insertAdjacentHTML(
+  if (!goods.length) {
+    cartWrapper.insertAdjacentHTML(
       "beforeend",
-      `
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+      `<div id="cart-empty">Ваша корзина пока пуста</div>`
+    );
+  } else {
+    goods.forEach((goodsItem) => {
+      cartWrapper.insertAdjacentHTML(
+        "beforeend",
+        `
             <div class="card" data-key="${goodsItem.id}">
             ${
               goodsItem.sale ? `<div class="card-sale">🔥Hot Sale🔥</div>` : ``
@@ -27,13 +30,13 @@ const renderGoods = (goods) => {
                       <h5 class="card-title">
                         ${goodsItem.title}
                       </h5>
-                      <button class="btn btn-primary">В корзину</button>
+                      <button class="btn btn-primary">Удалить</button>
                 </div>
             </div>
-        </div>
     `
-    );
-  });
+      );
+    });
+  }
 };
 
-export default renderGoods;
+export default renderCart;
